eab2b81d0109b80cbe2063ffe929f4e4975c46d4
reshifa <reshifa@gmail.com>
2015-05-30 17:01:59 +0300

    Start and Finish feature was improved

--- AramisIDE/Actions/CurrentDayPeriod.cs
+++ AramisIDE/Actions/CurrentDayPeriod.cs
@@ -12,7 +12,9 @@ namespace AramisIDE.Actions
         public CurrentDayPeriod()
             {
             var str = string.Format(@"declare @StartDate datetime2 = '{0}';
-declare @FinishDate datetime2 = '{0} 23:59:59.999';", DateTime.Now.ToString("yyyy-MM-dd"));
+declare @FinishDate datetime2 = '{0} 23:59:59.999';
+--	.[Date] between @StartDate and @FinishDate
+", DateTime.Now.ToString("yyyy-MM-dd"));
             ToClipboard(str);
             }
         }
